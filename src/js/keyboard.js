0bdfe229eb4d834d4a57918c0160e0470a9ac84b
leejh4197 <leejh4197@naver.com>
2022-06-24 02:30:13 +0900

    feat:darkmode

---
+++ src/js/keyboard.js
@@ -0,0 +1,26 @@
+export class Keyboard {
+  #switchEl;
+  #fontSelectEl;
+  constructor() {
+    this.#assignElement();
+    this.#addEvent();
+  }
+
+  #assignElement() {
+    this.#switchEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
+  }
+
+  #addEvent() {
+    this.#switchEl.addEventListener("change", (event) => {
+      document.documentElement.setAttribute(
+        "theme",
+        event.target.checked ? "dark-mode" : ""
+      );
+      console.log(event.target.checked);
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
+    });
+  }
+}
